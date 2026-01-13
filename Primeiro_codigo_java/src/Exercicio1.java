a334c2c1a5b537ece9c9e34a6340b0914a94526b
victorDev2030 <victor.pers1010@gmail.com>
2026-01-12 22:12:16 -0300

    First task in Java

---
+++ Primeiro_codigo_java/src/Exercicio1.java
@@ -0,0 +1,10 @@
+public class Exercicio1 {
+    public static void main(String[] args){
+        int idade1 = 20;
+        int idade2 = 3;
+        int finalIdade = (idade1-idade2);
+        System.out.println("a primeira idade é:"+idade1);
+        System.out.println("a segunda idade é:"+idade2);
+        System.out.println("a diferença da idade vai ser:"+finalIdade);
+    }
+}
---
+++ Primeiro_codigo_java/src/FirstCode.java
@@ -0,0 +1,6 @@
+public class FirstCode {
+    public static void main(String[] args){
+        int idade = 40;
+        System.out.println("Minha idade = "+idade);
+    }
+}
