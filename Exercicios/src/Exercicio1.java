0b8d9843e8001d72895114b4be337d21b9311b17
victorDev2030 <victor.pers1010@gmail.com>
2026-01-12 23:16:14 -0300

    Primeiro Exercicio Java

---
+++ Exercicios/src/Exercicio1.java
@@ -0,0 +1,8 @@
+public class Exercicio1 {
+    public static void main(String[] args){
+        int numero1 = 5;
+        int numero2 = 1;
+        int newNumber = (numero1-numero2);
+        System.out.println("A diferença de valores é de: "+newNumber);
+    }
+}
