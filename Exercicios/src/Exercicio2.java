91a7078c6c199b3b8e68555e12db7d49a1cac7f2
victorDev2030 <victor.pers1010@gmail.com>
2026-01-13 22:31:14 -0300

    Exercicio 2 do curso

---
+++ Exercicios/src/Exercicio2.java
@@ -0,0 +1,8 @@
+public class Exercicio2 {
+    public static void main(String[] args){
+        int salario = 2500;
+        float porcentagem = 0.7f;
+        float valorFinal = salario*porcentagem;
+        System.out.println("70% do Salario é: "+valorFinal);
+    }
+}
