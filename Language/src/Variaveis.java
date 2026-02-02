de3f901eb6af680cfbca5d8a40ef7d7258c9d79d
VictorDev2030 <victor.pers1010@gmail.com>
2026-02-01 21:07:02 -0300

    transicionando os arquivos

---
+++ Language/src/Variaveis.java
@@ -0,0 +1,26 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        int bicicleta = 1;
+        int pneu = 2;
+        int guidon = 1;
+
+        int inteiro = 100000;
+        float flutuar = 0.0f;
+        double duplo = 0.0d;
+        boolean boleano = true; /*Só pode ser true ou false*/
+
+        String mome = "Victor";
+        String invalido = null;
+
+        long cpf = 123_321_32_21;
+
+        System.out.println("ola \t amigo");
+        System.out.println("ola \n amigo");
+        System.out.println("ola \f amigo");
+        System.out.println("ola \r amigo");
+        System.out.println("ola \" amigo, \'");
+        System.out.println("ola \\ amigo");
+        System.out.println(invalido);
+        System.out.println("aqui está seu CPF: "+cpf);
+    }
+}
