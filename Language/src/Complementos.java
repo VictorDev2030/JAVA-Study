4cf31c2760a926f0e3fa8329ae8fedc8d2175c20
VictorDev2030 <victor.pers1010@gmail.com>
2026-02-06 20:47:45 -0300

    metodo var

---
+++ Language/src/Complementos.java
@@ -0,0 +1,15 @@
+import java.nio.file.Path;
+import java.util.List;
+
+public class Complementos {
+    public static void main(String[] args) {
+        /*var não pode ser colocado fora de um metodo */
+        var list = List.of("one");
+        for (var element: list){
+            System.out.println(element);
+        }
+
+        var nome = "Gabriel";
+        System.out.println(nome);
+    }
+}
