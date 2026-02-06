05a8c0865f43662cbfce16f403ea50d622a34cfa
VictorDev2030 <victor.pers1010@gmail.com>
2026-02-06 20:24:45 -0300

    terminando a calculadora primitiva

--- Projects/src/CalculadoraNoTerminal.java
+++ Projects/src/CalculadoraNoTerminal.java
@@ -4,7 +4,6 @@ public class CalculadoraNoTerminal {
         Scanner input = new Scanner(System.in);
         double resposta = 0, numero1 = 0, numero2 = 0;
         boolean validador = true;
-        String[] names = {"x", "-", "*", "/"};
 
         System.out.print("Hello User\n");
         System.out.println(" ");
@@ -12,36 +11,34 @@ public class CalculadoraNoTerminal {
         while(validador) {
             System.out.println(" ");
             System.out.println(
-                    "+. Somar \n" +
-                    "-. Subtrair \n" +
-                    "*. Multiplicar \n" +
-                    "/. Dividir \n" +
-                    "x. Exit");
+                    "1. Somar \n" +
+                    "2. Subtrair \n" +
+                    "3. Multiplicar \n" +
+                    "4. Dividir \n" +
+                    "5. Exit");
             System.out.println(" ");
             System.out.print("Me diga oque gostaria de fazer: ");
-            String usuario = input.next();
+            int usuario = input.nextInt();
 
-            if (usuario.equals(names)){
+            if (usuario >= 1 && usuario <= 4){
                 System.out.print("1. Numero: ");
                 numero1 = input.nextDouble();
                 System.out.print("2. Numero: ");
                 numero2 = input.nextDouble();
                 System.out.print("Números: " + numero1 + " e " + numero2 + "\n");
             }
-
-            switch (usuario) {
-                case "+": resposta = numero1 + numero2; break;
-                case "-": resposta = numero1 - numero2; break;
-                case "*": resposta = numero1 * numero2; break;
-                case "/": resposta = numero1 / numero2; break;
-                case "x": validador = false;
-            }
-            if (usuario == names[]){
-                System.out.println("Resultado = " + resposta);
-            }
-            else if (usuario == "x"){
+            else{
                 System.out.println("Fechando o programa...");
+                System.exit(0);
             }
+            switch (usuario) {
+                case 1: resposta = numero1 + numero2; break;
+                case 2: resposta = numero1 - numero2; break;
+                case 3: resposta = numero1 * numero2; break;
+                case 4: resposta = numero1 / numero2; break;
+                case 5: validador = false;
+            }
+            System.out.println("Resultado = " + resposta);
 
             try {
                 Thread.sleep(1000);
